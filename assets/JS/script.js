9fe172bbe3998ff97201e3fbf96ea996e248efac
Precious Carbonell <155498709+Precious-Carbonell@users.noreply.github.com>
2024-03-16 09:31:21 +0800

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,12 +9,12 @@ button.addEventListener('click', () => {
 
     if (clickCount < 12) {
         // Play 'wedbell.mp3' for the first 11 clicks
-        const audio = new Audio('../assets/sounds/wedbell.mp3');
+        const audio = new Audio('../sounds/wedbell.mp3');
         audio.play();
     } else if (clickCount === 12) {
         // After 12 clicks, replace the background and play 'wedding.mp4'
-        container.style.background = 'url("../assets/images/church.jpg") center/cover no-repeat fixed';
-        container.innerHTML = '<video id="_video" width="100%" height="100%" controls autoplay><source src="../assets/video/wedding.mp4" type="video/mp4"></video>';
+        container.style.background = 'url("../images/church.jpg") center/cover no-repeat fixed';
+        container.innerHTML = '<video id="_video" width="100%" height="100%" controls autoplay><source src="../video/wedding.mp4" type="video/mp4"></video>';
     }
 
     // Update the notification count
